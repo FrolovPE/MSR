678cc5f68fac8cd023ab49b08a33237f05384f68
FrolovPE <UchFrolovP.com>
2026-04-13 17:27:31 -0400

    'Mon, 13 Apr 2026 17:27:31  '

--- msr.cpp
+++ msr.cpp
@@ -18,6 +18,105 @@ int get_len_msr(int nx, int ny)
 {
     return (nx - 1) * (ny - 1) * 6 + (nx - 1) * 4 * 2 + (ny - 1) * 4 * 2 + 3 * 2 + 2 * 2;
 }
+void ij2l(int nx, int ny, int i, int j, int &l)
+{
+    ny = ny;
+    l = i + j * (nx + 1);
+}
+void l2ij(int nx, int ny, int l, int &i, int &j)
+{
+    ny = ny;
+    j = l / (nx + 1);
+    i = l - j * (nx + 1);
+}
+
+int IA_ij(int nx, int ny, double hx, double hy, int i, int j, int is, int js, int s, int *I, double *A)
+{
+    double S = 0.5 * hx * hy;
+    int l, ls,sj;
+
+    ij2l(nx, ny, i, j, l);
+    ij2l(nx, ny, is, js, ls);
+
+    // if (l != ls)
+    // I[s] = ls;
+
+    if(!A && !I)
+    {
+        printf("A and I are nullptrs in IA_ij for i = %d , j = %d , is = %d , js = %d , s = %d\n",i,j,is,js,s);
+        return -1;
+    }
+
+    if(i > 0 && i < nx && j > 0 && j < ny)
+    {
+        if (l  == ls) A[s] = 6 * S / 6;
+        else A[s] = 2 * S / 12;
+    }
+
+    if(i > 0 && i < nx && j == 0)
+    {
+        if (l == ls) A[s] = 3 * S / 6;
+        else if (s == 0 || s == 1) A[s] = 1 * S / 12;
+        else if (s == 2 || s == 3) A[s] = 2 * S / 12;
+        else return -1;
+    }
+
+    if(i > 0 && i < nx && j == ny)
+    {
+        if (l == ls) A[s] = 3 * S / 6;
+        else if (s == 0 || s == 3) A[s] = 1 * S / 12;
+        else if (s == 1 || s == 2) A[s] = 2 * S / 12;
+        else return -1;
+    }
+
+    if(i == 0 && j > 0 && j < ny)
+    {
+        if (l == ls) A[s] = 3 * S / 6;
+        else if (s == 0 || s == 3) A[s] = 2 * S / 12;
+        else if (s == 1 || s == 2) A[s] = 1 * S / 12;
+        else return -1;
+    }
+
+    if(i == nx && j > 0 && j < ny)
+    {
+        if (l == ls) A[s] = 3 * S / 6;
+        else if (s == 0 || s == 3) A[s] = 1 * S / 12;
+        else if (s == 1 || s == 2) A[s] = 2 * S / 12;
+        else return -1;
+    }
+
+    if(i == 0 && j == 0)
+    {
+        if (l == ls) A[s] = 2 * S / 6;
+        else if (s == 0 || s == 1) A[s] = 1 * S / 12;
+        else if (s == 2) A[s] = 2 * S / 12;
+        else return -1;
+    }
+
+    if(i == nx && j == ny)
+    {
+        if (l == ls) A[s] = 2 * S / 6;
+        else if (s == 0 || s == 2) A[s] = 1 * S / 12;
+        else if (s == 1) A[s] = 2 * S / 12;
+        else return -1;
+    }
+
+    if(i == 0 && j == ny)
+    {
+        if (l == ls) A[s] = 1 * S / 6;
+        else if (s == 0 || s == 1) A[s] = 1 * S / 12;
+        else return -1;
+    }
+
+    if(i == nx && j == 0)
+    {
+        if (l == ls) A[s] = 1 * S / 6;
+        else if (s == 0 || s == 1) A[s] = 1 * S / 12;
+        else return -1;
+    }
+
+    return 0;
+}
 
 void* msr_sovle(void* ptr)
 {
@@ -36,6 +135,8 @@ void* msr_sovle(void* ptr)
     int thr = arg->thr;
     double *A = arg->A;
     int *I = arg->I;
+
+
 
 
     return nullptr;
--- msr.h
+++ msr.h
@@ -10,5 +10,8 @@ void get_my_rows(int n, int p, int thr, int &i1, int &i2);
 void report(const char *name, int task, double r1, double r2, double r3, double r4, double t1, double t2,int it, double eps, int k, int nx, int ny, int p);
 int get_len_msr(int nx, int ny);
 void* msr_sovle(void* ptr);
+void ij2l(int nx, int ny, int i, int j, int &l);
+void l2ij(int nx, int ny, int l, int &i, int &j);
+int IA_ij(int nx, int ny, double hx, double hy, int i, int j, int is, int js, int s, int *I = nullptr, double *A = nullptr);
 
 #endif
